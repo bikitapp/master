c900067627df9be9691d64f0488d0a4e2cbf6312
jleyton <leyton.javier@gmail.com>
2016-10-24 22:51:13 -0300

    Versión de prueba

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { Routes } from '@angular/router';
+
+@Component({
+    selector: "app",
+    template: "<page-router-outlet></page-router-outlet>"
+})
+export class AppComponent {
+
+}
